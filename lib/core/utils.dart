06d23ee2b7821da7217e41d513af3fee88913fec
step-be-step <zasweq41@gmail.com>
2023-03-03 09:05:12 +0300

    picking multiply images & displaying in carousel

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void showSnackBar(BuildContext context, String content) {
   ScaffoldMessenger.of(context).showSnackBar(
@@ -10,4 +13,17 @@ void showSnackBar(BuildContext context, String content) {
 
 String getNameFromEmail(String email) {
   return email.split('@')[0];
+}
+
+Future<List<File>> pickImages() async {
+  List<File> images = [];
+  final ImagePicker picker = ImagePicker();
+  final pickedFiles = await picker.pickMultiImage();
+  if (pickedFiles.isNotEmpty) {
+    for (final image in pickedFiles) {
+      images.add(File(image.path));
+    }
+  }
+
+  return images;
 }
--- lib/features/post/views/create_post_view.dart
+++ lib/features/post/views/create_post_view.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:socbp/common/common.dart';
-import 'package:socbp/common/rounded_small_button.dart';
+import 'package:socbp/constants/constants.dart';
+import 'package:socbp/core/utils.dart';
 import 'package:socbp/features/auth/controller/auth_controller.dart';
 import 'package:socbp/theme/pallete.dart';
 
@@ -15,6 +20,20 @@ class CreaetePostScreen extends ConsumerStatefulWidget {
 }
 
 class _CreaetePostScreenState extends ConsumerState<CreaetePostScreen> {
+  final postTextController = TextEditingController();
+  List<File> images = [];
+
+  @override
+  void dispose() {
+    super.dispose();
+    postTextController.dispose();
+  }
+
+  Future<void> onPickImages() async {
+    images = await pickImages();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     final currentUser = ref.watch(currentUserDetailsProvider).value;
@@ -22,11 +41,10 @@ class _CreaetePostScreenState extends ConsumerState<CreaetePostScreen> {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-          onPressed: () {},
-          icon: const Icon(
-            Icons.close,
-            size: 30,
-          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.close, size: 30),
         ),
         actions: [
           RoundedSmallButton(
@@ -41,18 +59,80 @@ class _CreaetePostScreenState extends ConsumerState<CreaetePostScreen> {
           ? const Loader()
           : SafeArea(
               child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      CircleAvatar(
-                        backgroundImage: NetworkImage(currentUser.profilePic),
-                      )
-                    ],
-                  )
-                ],
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          backgroundImage: NetworkImage(currentUser.profilePic),
+                          radius: 30,
+                        ),
+                        const SizedBox(width: 15),
+                        Expanded(
+                          child: TextField(
+                            controller: postTextController,
+                            style: const TextStyle(
+                              fontSize: 22,
+                            ),
+                            decoration: const InputDecoration(
+                                hintText: "Расскажи что нибудь",
+                                hintStyle: TextStyle(
+                                  color: Pallete.greyColor,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                                border: InputBorder.none),
+                            maxLines: null,
+                          ),
+                        )
+                      ],
+                    ),
+                    if (images.isNotEmpty)
+                      CarouselSlider(
+                        items: images.map((file) {
+                          return Container(
+                            width: MediaQuery.of(context).size.width,
+                            margin: const EdgeInsets.symmetric(horizontal: 5),
+                            child: Image.file(file),
+                          );
+                        }).toList(),
+                        options: CarouselOptions(
+                          height: 400,
+                          enableInfiniteScroll: false,
+                        ),
+                      ),
+                  ],
+                ),
               ),
-            )),
+            ),
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.only(bottom: 10),
+        decoration: const BoxDecoration(
+          border: Border(
+              top: BorderSide(
+            color: Pallete.greyColor,
+            width: 0.3,
+          )),
+        ),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0).copyWith(left: 15, right: 15),
+              child: GestureDetector(
+                  onTap: onPickImages,
+                  child: SvgPicture.asset(AssetsConstants.galleryIcon)),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0).copyWith(left: 15, right: 15),
+              child: SvgPicture.asset(AssetsConstants.gifIcon),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0).copyWith(left: 15, right: 15),
+              child: SvgPicture.asset(AssetsConstants.emojiIcon),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
